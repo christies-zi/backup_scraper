922fd0d0f3c938c02119ac68aafbb2ee66ad0c8a
Kristina Zimina <kristina.zimina21@imperial.ac.uk>
2025-06-22 14:39:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
 
 app = Flask(__name__)
